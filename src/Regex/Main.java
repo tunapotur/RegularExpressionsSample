2dc1bf048ba2bba9f4a082bbf0796a608c168c11
tpotur <tpotur@gmail.com>
2022-08-23 10:49:10 +0300

    Regex Example

--- src/Regex/Main.java
+++ src/Regex/Main.java
@@ -1,7 +1,17 @@
 package Regex;
 
+import java.util.regex.*;
+
 public final class Main {
-    public static void main(String[] args){
-        System.out.println("Init Commit Test");
+    public static void main(String[] args) {
+        String articleToJournalAndSeries = "\\w+:\\w+,\\w+,\\w+ [\\w ]+";
+        System.out.println(Pattern.matches(articleToJournalAndSeries, "TACO:tacotaco,2012,Great Architecture"));
+        System.out.println(Pattern.matches(articleToJournalAndSeries, "TSE:abc2017,2017,Classes Methods and More"));
+        System.out.println(Pattern.matches(articleToJournalAndSeries, "TSE:mvp2015,2015,Model Consistency"));
+        System.out.println(Pattern.matches(articleToJournalAndSeries, "TSE:mvp2016,2016,Better Model Consistency"));
+        System.out.println(Pattern.matches(articleToJournalAndSeries, "ICSA:rr2017,2017,Components still have no interfaces"));
+        System.out.println(Pattern.matches(articleToJournalAndSeries, "ICSME:component2022,2022,Sorting Components"));
+        System.out.println(Pattern.matches(articleToJournalAndSeries, "ICSME:evo22,2022,Software Evolution"));
+
     }
 }
